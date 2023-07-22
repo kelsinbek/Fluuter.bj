48edf157fba44355356d02e7fe188f4e5a2471f2
kelsinbek <janaevkelsinbek@gmail.com>
2023-07-23 01:23:55 +0600

    i finished 1/3 part of weather_app

--- hw_9_weather_app/lib/constants/app_text_style.dart
+++ hw_9_weather_app/lib/constants/app_text_style.dart
@@ -4,8 +4,8 @@ class AppTextStyles {
   static const titleStyle =
       TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w800);
 
-  static const sanTextStyle =
-      TextStyle(fontSize: 40, fontWeight: FontWeight.w800);
+  static const sanTextStyle = TextStyle(
+      color: Colors.white, fontSize: 100, fontWeight: FontWeight.w500);
 
   static const calculateStyle =
       TextStyle(fontSize: 22, fontWeight: FontWeight.w500);
--- hw_9_weather_app/lib/view/home_view.dart
+++ hw_9_weather_app/lib/view/home_view.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hw_9_weather_app/constants/app_colors.dart';
+import 'package:hw_9_weather_app/constants/app_text_style.dart';
 
 class MyHomeView extends StatefulWidget {
   const MyHomeView({super.key});
@@ -17,11 +20,79 @@ class _MyHomeViewState extends State<MyHomeView> {
         title: const Center(
           child: Text(
             'HomeWork 09',
-            style: TextStyle(
-                color: Colors.black, fontSize: 24, fontWeight: FontWeight.w800),
+            style: AppTextStyles.titleStyle,
           ),
         ),
         elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/image/bg_image.jpg'),
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Icon(
+                    Icons.near_me,
+                    color: AppColors.iconColor,
+                  ),
+                  Icon(
+                    Icons.location_city,
+                    color: AppColors.iconColor,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                ),
+                const Text(
+                  '8',
+                  style: AppTextStyles.sanTextStyle,
+                ),
+                Image.asset(
+                  'assets/image/cloud.png',
+                  height: 120,
+                  width: 150,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            const Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                "You'll\n need\n and ",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 70,
+                ),
+              ),
+            ),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                "Bishkek",
+                style: TextStyle(color: Colors.white, fontSize: 70),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
